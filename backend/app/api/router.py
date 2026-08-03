5fbe52d66468abddb5b1db466cba2d4f875ce689
afmsilva13-web <afmsilva13@gmail.com>
2026-08-03 01:06:28 +0100

    Update router.py

--- backend/app/api/router.py
+++ backend/app/api/router.py
@@ -1,8 +1,12 @@
-
 from fastapi import APIRouter
+from app.api.routes import reservoirs
 
 api_router = APIRouter()
 
+api_router.include_router(reservoirs.router)
+
 @api_router.get("/")
 def root():
-    return {"message":"Welcome to AQUAGRID"}
+    return {
+        "message": "Welcome to AQUAGRID"
+    }
